3d62a19b39b9225aaeebe55a3bd2a30394e40705
Oyelola Adeboye <78259921+Boye-dev@users.noreply.github.com>
2024-08-16 14:36:20 +0100

    feat: removed password from repo

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
-export const sequelize = new Sequelize("snapnet_db", "root", "Redeem070.", {
+export const sequelize = new Sequelize("snapnet_db", "root", process.env.DB_PASSWORD, {
   host: "localhost",
   dialect: "mysql",
 });
